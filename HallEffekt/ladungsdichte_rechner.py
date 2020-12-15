8ad9ba253139b04c8dd37f7639bece682bd736e2
Tim Theissel <tim.theissel@gmail.com>
2020-12-16 00:41:21 +0100

    Hilfe Mord

--- HallEffekt/ladungsdichte_rechner.py
+++ HallEffekt/ladungsdichte_rechner.py
@@ -1,3 +1,4 @@
+import math
 import numpy as np 
 import uncertainties as unc 
 import uncertainties.unumpy as unp 
@@ -18,7 +19,7 @@ d = ufloat(1.8e-8,1.8e-11)
 
 n= -(Ip*Tp)/(Up*d*const.e)
 n2= -(Im*Tm)/(Um*d*const.e)
-print(n)
+#print(n)
 #print(n2)
 
 
@@ -37,3 +38,15 @@ tau = (2*const.m_e)/(n*const.e**2*rho)
 j=1
 vd = (-n*const.e)/j
 print(vd)
+
+mu = -(const.e*tau)/(2*const.m_e)
+#print(mu)
+
+EF=const.h*(3/(8*math.pi)**(1/3))/(2*const.m_e)
+#print(EF)
+
+v = ((2*EF)/const.m_e)
+#print(v)
+
+l = -tau * v
+#print(l)
