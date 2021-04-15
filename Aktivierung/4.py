7124a8205a1c8c02849b16208177909a32f37416
Tim Theissel <tim.theissel@gmail.com>
2021-04-15 12:53:26 +0200

    oops

---
+++ Aktivierung/4.py
@@ -0,0 +1,23 @@
+import math
+import numpy as np 
+import matplotlib.pyplot as plt 
+import uncertainties as unc 
+import uncertainties.unumpy as unp 
+from uncertainties import ufloat
+import scipy.constants as const
+
+
+a = 261
+b = 236
+c = 224.4
+print((c-a)/(-c))
+print((c-b)/(-c))
+
+
+d = 45.5
+e = 42.3
+print((e-d)/(-e))
+
+f = 260
+g = 260.4
+print((g-f)/g)
