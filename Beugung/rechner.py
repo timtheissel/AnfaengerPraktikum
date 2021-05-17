d116826270919e72aa1f8b32313f30527fdfe97a
Tim Theissel <tim.theissel@gmail.com>
2021-05-17 05:33:13 +0200

    kurzen Allnighter geschoben

--- Beugung/rechner.py
+++ Beugung/rechner.py
@@ -12,8 +12,8 @@ a1 = (a * np.pi)/ 180
 b1 = (b * np.pi)/ 180 
 
 n= np.sin(a1)/np.sin(b1)
-print(n)
-print(np.std(n))
+#print(n)
+#print(np.std(n))
 
 n1 = ufloat(1.494, 0.019)
 
@@ -22,10 +22,10 @@ print(299792458/n1)
 d = 5.85 
 
 s = (d * np.sin(a1-b1))/np.cos(b1) 
-print(s)
+#print(s)
 
 b2 = unp.arcsin(np.sin(b1)/n1)
-print(b2/np.pi * 180)
+#print(b2/np.pi * 180)
 
 s1 = (d * unp.sin(a1-b2))/unp.cos(b2) 
-print(s1)
+#print(s1)
